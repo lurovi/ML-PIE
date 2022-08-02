27def4ccc921ce6bd47b2d10c2e07165dd3a6d17
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2022-08-02 10:32:18 +0200

    Sample problem updated and small bug fixed.

--- gp/nsga2/SampleProblem.py
+++ gp/nsga2/SampleProblem.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from pymoo.core.problem import ElementwiseProblem
 
 from gp.tree.PrimitiveTree import PrimitiveTree
@@ -8,8 +9,8 @@ class SampleProblem(ElementwiseProblem):
 
     def __init__(self):
         super().__init__(n_var=1, n_obj=2, n_ieq_constr=0)
-        self.target_function = lambda x: np.sin(x) + np.sin(x ** 2) + np.sin(x) * np.cos(x) + np.random.normal(
-            scale=0.1, size=1)
+        self.target_function = lambda x: math.sin(x) + math.sin(x ** 2) + math.sin(x) * math.cos(x) + np.random.normal(
+            scale=0.1, size=1)[0]
         self.interval_lower_bound = -3
         self.interval_upper_bound = 3
         self.step = 0.1
@@ -20,8 +21,7 @@ class SampleProblem(ElementwiseProblem):
         tree = x[0]
         for i in np.arange(self.interval_lower_bound, self.interval_upper_bound, self.step):
             target = self.target_function(i)
-            # value = tree.compile([i])
-            value = self.target_function(i) + np.random.normal(scale=0.1, size=1)
+            value = tree.compile([i])
             square_error = (value - target) ** 2
             total_error = total_error + square_error
             total_evaluations = total_evaluations + 1
