751c7b17e3e0c6e482143ddf319c867c548bdbce
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2022-09-15 15:10:46 +0200

    FeedbackThread added, it extends StoppableThread.

---
+++ threads/FeedbackThread.py
@@ -0,0 +1,17 @@
+from nsgp.interpretability.InterpretabilityEstimateUpdater import InterpretabilityEstimateUpdater
+from threads.StoppableThread import StoppableThread
+
+
+class FeedbackThread(StoppableThread):
+    def __init__(self, interpretability_estimate_updater: InterpretabilityEstimateUpdater, delay: float = None, *args,
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        self.interpretability_estimate_updater = interpretability_estimate_updater
+        self.delay = delay
+
+    def run(self) -> None:
+        while not self.stopped():
+            if self.delay is not None and self.delay > 0:
+                self.interpretability_estimate_updater.delayed_update(self.delay)
+            else:
+                self.interpretability_estimate_updater.immediate_update()
