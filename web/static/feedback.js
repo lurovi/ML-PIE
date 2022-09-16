b89b9fc47c06653452f65a17c4254e4275f57f6f
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2022-09-16 18:24:22 +0200

    Primitive web functionality added, still needs adjusting.

---
+++ web/static/feedback.js
@@ -0,0 +1,41 @@
+$("document").ready(function() {
+    retrieveModels();
+});
+
+var divModelsContainer = $(".model-container")
+
+divModelsContainer.click(function() {
+    divModelsContainer.each(function () {
+        $(this).removeClass("selected");
+    });
+    $(this).addClass("selected");
+    let i = parseInt($("input[type=hidden]", this).val());
+    let feedback = -1 + 2*i;
+    provideFeedback(feedback);
+    retrieveModels();
+});
+
+function retrieveModels(){
+    $("#div-loading-img").attr("hidden", false)
+    $.ajax({
+      url: "getData",
+      headers: { 'x-access-tokens': localStorage.getItem("token") }
+    }).done(data => {
+      formula_latex = $("h4.mb-0");
+      formula_latex[0].innerHTML = data.t1;
+      formula_latex[1].innerHTML = data.t2;
+    });
+    $("#div-loading-img").attr("hidden", true)
+}
+
+function provideFeedback(feedback){
+    $.ajax({
+      type: "POST",
+      url: "provideFeedback",
+      headers: { 'x-access-tokens': localStorage.getItem("token") },
+      data: JSON.stringify({ 'feedback': feedback }),
+      contentType: "application/json"
+    }).done(data => {
+      console.log(data.message);
+    });
+}
---
+++ web/static/init.js
@@ -0,0 +1,6 @@
+$("#btn-proceed").on("click",function() {
+    $.ajax({
+      url: "startRun"
+    }).done(data => localStorage.setItem("token", data.id));
+    window.location = window.location = window.location.protocol + "//" + window.location.host + "/" + "feedback";
+})
