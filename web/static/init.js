fec6ef2473947b7ee5779433130b272e2ddd74d3
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2022-09-17 12:44:33 +0200

    Small bug fixed.

--- web/static/init.js
+++ web/static/init.js
@@ -1,6 +1,9 @@
 $("#btn-proceed").on("click",function() {
     $.ajax({
       url: "startRun"
-    }).done(data => localStorage.setItem("token", data.id));
-    window.location = window.location = window.location.protocol + "//" + window.location.host + "/" + "feedback";
+    }).done( data => {
+          localStorage.setItem("token", data.id);
+          window.location = window.location = window.location.protocol + "//" + window.location.host + "/" + "feedback";
+        }
+    );
 })
