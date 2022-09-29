826645d5d1aa17bca99662bb20e9389f1d4aebbe
Luigi Rovito <luigirovito2@gmail.com>
2022-09-29 16:54:21 +0200

    [dev] Friedman1 dataset seed fixed

--- exps/benchmark/FriedmanPreProcessing.py
+++ exps/benchmark/FriedmanPreProcessing.py
@@ -8,8 +8,12 @@ if __name__ == "__main__":
     random.seed(42)
     np.random.seed(42)
     X_train, y_train = make_friedman1(n_samples=700, n_features=7, noise=0.25, random_state=42)
-    X_dev, y_dev = make_friedman1(n_samples=400, n_features=7, noise=0.25, random_state=42)
-    X_test, y_test = make_friedman1(n_samples=200, n_features=7, noise=0.25, random_state=42)
+    random.seed(43)
+    np.random.seed(43)
+    X_dev, y_dev = make_friedman1(n_samples=400, n_features=7, noise=0.25, random_state=43)
+    random.seed(44)
+    np.random.seed(44)
+    X_test, y_test = make_friedman1(n_samples=200, n_features=7, noise=0.25, random_state=44)
 
     PicklePersist.compress_pickle("friedman1", {"training": (X_train, y_train),
                                                  "validation": (X_dev, y_dev),
