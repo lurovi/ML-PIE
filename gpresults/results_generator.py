a32d24882c362f0b1e07b7758e414a495da28fcc
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2022-11-02 13:20:24 +0100

    Traditional gp results updated for survey.

--- gpresults/results_generator.py
+++ gpresults/results_generator.py
@@ -3,8 +3,6 @@ import re
 import pandas as pd
 from sympy import latex, parse_expr
 
-from threads.MlPieRun import MlPieRun
-
 
 def latex_format(readable_repr: str) -> str:
     try:
@@ -15,22 +13,29 @@ def latex_format(readable_repr: str) -> str:
 
 
 folder = '../exps/test_results_gp_traditional_dropout'
-dataset = 'heating'
-model = 'elastic_model'
-target_model = 'phi'
+datasets = ['heating', 'boston']
+models = ['elastic_model', 'n_nodes']
+target_models = ['phi', 'size']
 
-dataframes = []
+for dataset in datasets:
+    for k in range(len(models)):
+        model = models[k]
+        target_model = target_models[k]
+        for j in range(3):
+            split_seed = 40 + j
+            dataframes = []
 
-for i in range(10):
-    seed = 700 + i
-    filename = folder + '/best-' + dataset + '-' + model + '-GPT_' + str(seed) + '.csv'
-    dataframes.append(pd.read_csv(filename))
+            for i in range(10):
+                seed = 700 + i
+                filename = folder + '/best-' + dataset + '-' + model + '-GPT_' + str(seed) + '_' + str(
+                    split_seed) + '.csv'
+                dataframes.append(pd.read_csv(filename))
 
-df = pd.concat(dataframes)
-df.drop(columns=df.columns[0], axis=1, inplace=True)
-df = df.rename(columns={"latex_tree": "readable_tree"})
-df["latex_tree"] = df["readable_tree"].map(latex_format)
-df["problem"] = dataset
+            df = pd.concat(dataframes)
+            df.drop(columns=df.columns[0], axis=1, inplace=True)
+            df = df.rename(columns={"latex_tree": "readable_tree"})
+            df["latex_tree"] = df["readable_tree"].map(latex_format)
+            df["problem"] = dataset
 
-target_file = dataset + '_' + target_model + '.csv'
-df.to_csv(target_file, index=False)
+            target_file = dataset + '_' + target_model + '_' + str(split_seed) + '.csv'
+            df.to_csv(target_file, index=False)
